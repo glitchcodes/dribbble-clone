0bb7349490f139d63b9be5a29f456475767db7dc
Vincent Tampos <56838797+glitchcodes@users.noreply.github.com>
2024-02-19 07:03:24 +0000

    Add conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,53 +1,177 @@
-import 'bootstrap/dist/css/bootstrap.css'
-import './App.css';
+import "bootstrap/dist/css/bootstrap.css";
+import "./App.css";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Card from "./components/Card";
 
 function App() {
-    const data = [
-        {"image":"https://http.cat/100.jpg","name":"llelande0","likes":33,"views":636},
-        {"image":"https://http.cat/101.jpg","name":"emansuer1","likes":19,"views":336},
-        {"image":"https://http.cat/102.jpg","name":"abonifacio2","likes":414,"views":390},
-        {"image":"https://http.cat/103.jpg","name":"rbrownstein3","likes":196,"views":803},
-        {"image":"https://http.cat/200.jpg","name":"vrushworth4","likes":76,"views":705},
-        {"image":"https://http.cat/201.jpg","name":"bgierke5","likes":265,"views":687},
-        {"image":"https://http.cat/202.jpg","name":"gdod6","likes":295,"views":381},
-        {"image":"https://http.cat/203.jpg","name":"tfinnie7","likes":356,"views":565},
-        {"image":"https://http.cat/204.jpg","name":"jqueenborough8","likes":302,"views":759},
-        {"image":"https://http.cat/205.jpg","name":"bnormavell9","likes":403,"views":954},
-        {"image":"https://http.cat/206.jpg","name":"gpoulgreena","likes":157,"views":431},
-        {"image":"https://http.cat/207.jpg","name":"tdumphreyb","likes":164,"views":793},
-        {"image":"https://http.cat/208.jpg","name":"mstockerc","likes":145,"views":716},
-        {"image":"https://http.cat/226.jpg","name":"ssticheld","likes":156,"views":607},
-        {"image":"https://http.cat/300.jpg","name":"gyeabsleye","likes":58,"views":180},
-        {"image":"https://http.cat/301.jpg","name":"mpoilef","likes":61,"views":171},
-        {"image":"https://http.cat/302.jpg","name":"hhookg","likes":445,"views":43},
-        {"image":"https://http.cat/303.jpg","name":"fmonellih","likes":311,"views":422},
-        {"image":"https://http.cat/304.jpg","name":"ipiggoti","likes":305,"views":529},
-        {"image":"https://http.cat/305.jpg","name":"bkocherj","likes":43,"views":135}
-    ]
+  const data = [
+    {
+      image: "https://http.cat/100.jpg",
+      name: "llelande0",
+      likes: 33,
+      views: 636,
+      is_liked: false,
+    },
+    {
+      image: "https://http.cat/101.jpg",
+      name: "emansuer1",
+      likes: 19,
+      views: 336,
+      is_liked: true,
+    },
+    {
+      image: "https://http.cat/102.jpg",
+      name: "abonifacio2",
+      likes: 414,
+      views: 390,
+      is_liked: false,
+    },
+    {
+      image: "https://http.cat/103.jpg",
+      name: "rbrownstein3",
+      likes: 196,
+      views: 803,
+      is_liked: false,
+    },
+    {
+      image: "https://http.cat/200.jpg",
+      name: "vrushworth4",
+      likes: 76,
+      views: 705,
+      is_liked: false,
+    },
+    {
+      image: "https://http.cat/201.jpg",
+      name: "bgierke5",
+      likes: 265,
+      views: 687,
+      is_liked: true,
+    },
+    {
+      image: "https://http.cat/202.jpg",
+      name: "gdod6",
+      likes: 295,
+      views: 381,
+      is_liked: false,
+    },
+    {
+      image: "https://http.cat/203.jpg",
+      name: "tfinnie7",
+      likes: 356,
+      views: 565,
+      is_liked: false,
+    },
+    {
+      image: "https://http.cat/204.jpg",
+      name: "jqueenborough8",
+      likes: 302,
+      views: 759,
+      is_liked: false,
+    },
+    {
+      image: "https://http.cat/205.jpg",
+      name: "bnormavell9",
+      likes: 403,
+      views: 954,
+      is_liked: false,
+    },
+    {
+      image: "https://http.cat/206.jpg",
+      name: "gpoulgreena",
+      likes: 157,
+      views: 431,
+      is_liked: false,
+    },
+    {
+      image: "https://http.cat/207.jpg",
+      name: "tdumphreyb",
+      likes: 164,
+      views: 793,
+      is_liked: true,
+    },
+    {
+      image: "https://http.cat/208.jpg",
+      name: "mstockerc",
+      likes: 145,
+      views: 716,
+      is_liked: false,
+    },
+    {
+      image: "https://http.cat/226.jpg",
+      name: "ssticheld",
+      likes: 156,
+      views: 607,
+      is_liked: false,
+    },
+    {
+      image: "https://http.cat/300.jpg",
+      name: "gyeabsleye",
+      likes: 58,
+      views: 180,
+      is_liked: true,
+    },
+    {
+      image: "https://http.cat/301.jpg",
+      name: "mpoilef",
+      likes: 61,
+      views: 171,
+      is_liked: false,
+    },
+    {
+      image: "https://http.cat/302.jpg",
+      name: "hhookg",
+      likes: 445,
+      views: 43,
+      is_liked: false,
+    },
+    {
+      image: "https://http.cat/303.jpg",
+      name: "fmonellih",
+      likes: 311,
+      views: 422,
+      is_liked: false,
+    },
+    {
+      image: "https://http.cat/304.jpg",
+      name: "ipiggoti",
+      likes: 305,
+      views: 529,
+      is_liked: true,
+    },
+    {
+      image: "https://http.cat/305.jpg",
+      name: "bkocherj",
+      likes: 43,
+      views: 135,
+      is_liked: false,
+    },
+  ];
 
-    const cards = data.map((card) => {
-        return (
-            <div className="col-md-3">
-                <Card name={card.name} image={card.image} likes={card.likes} views={card.views} />
-            </div>
-        )
-    })
-
+  const cards = data.map((card) => {
     return (
-        <div className="App">
-            <Navbar />
-            <Hero />
-
-            <div className="container mt-5">
-                <div className="row">
-                    {cards}
-                </div>
-            </div>
-        </div>
+      <div className="col-md-3">
+        <Card
+          name={card.name}
+          image={card.image}
+          likes={card.likes}
+          views={card.views}
+          is_liked={card.is_liked}
+        />
+      </div>
     );
+  });
+
+  return (
+    <div className="App">
+      <Navbar />
+      <Hero />
+
+      <div className="container mt-5">
+        <div className="row">{cards}</div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,22 +1,42 @@
 function Card(props) {
-    return (
-        <div className="card mb-3">
-            <img src={props.image} className="card-img-top" alt="..." width="100" height="300" />
-                <div className="card-body">
-                    <div className="d-flex align-items-center">
-                        <p className="card-text w-100 mb-0 text-start">{props.name}</p>
-                        <span>
-                            <img src="/icons/heart.svg" width="15"/>
-                            {props.likes}
-                        </span>
-                        <span className="ms-2">
-                            <img src="/icons/eye.svg" width="15"/>
-                            {props.views}
-                        </span>
-                    </div>
-                </div>
+  const likeIcon = props.is_liked ? (
+    <img src="/icons/heart-liked.svg" width="15" />
+  ) : (
+    <img src="/icons/heart.svg" width="15" />
+  );
+
+  const isHotItem =
+    props.views >= 500 ? (
+      <span class="badge rounded-pill text-bg-warning position-absolute top-0 start-0 ms-2 mt-2">
+        Hot
+      </span>
+    ) : null;
+
+  return (
+    <div className="card mb-3 position-relative">
+      {isHotItem}
+      <img
+        src={props.image}
+        className="card-img-top"
+        alt="..."
+        width="100"
+        height="300"
+      />
+      <div className="card-body">
+        <div className="d-flex align-items-center">
+          <p className="card-text w-100 mb-0 text-start">{props.name}</p>
+          <span>
+            {likeIcon}
+            {props.likes}
+          </span>
+          <span className="ms-2">
+            <img src="/icons/eye.svg" width="15" />
+            {props.views}
+          </span>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default Card;
